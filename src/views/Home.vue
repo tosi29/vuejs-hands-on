27faea2c1054e58c996545ca9bc90df6975af965
tosi29 <tosi16@gmail.com>
2021-03-07 17:54:54 +0900

    Replace Home.vue with form and table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="home">
+    <!-- 
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    -->
+    <h3>検索条件</h3>
+    <div>
+      <label>User Name</label><input type="text" v-model="message" placeholder="Type user name to find">
+      <button>Search</button>
+    </div>
+    <h3>検索結果</h3>
+    <table border="1">
+      <thead>
+        <tr>
+          <td>User Id</td>
+          <td>User Namme</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><a href="detail.html">001</a></td>
+          <td>Tanaka</td>
+        </tr>
+        <tr>
+          <td><a href="detail.html">002</a></td>
+          <td>Tamura</td>
+        </tr>
+        <tr>
+          <td><a href="detail.html">003</a></td>
+          <td>Nakata</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
